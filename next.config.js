d42e1fd0ea7076f7d0850aa428b342a6120f6f46
Christian Escalante <chescalante.ar@gmail.com>
2021-01-07 15:45:15 -0300

    last image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   images: {
+    imageSizes: [320, 560], // widths
+    deviceSizes: [640, 3840],
     domains: ["lh3.googleusercontent.com", "drinx-api.herokuapp.com"],
   },
 };
