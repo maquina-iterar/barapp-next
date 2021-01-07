6aa5038f2613a2cb9a4c21e251e317d638cf3ae9
Christian Escalante <chescalante.ar@gmail.com>
2021-01-07 16:11:02 -0300

    fix material ui server render

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,13 @@ import Head from "next/head";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  // useEffect(() => {
-  //   // Remove the server-side injected CSS.
-  //   const jssStyles = document.querySelector("#jss-server-side");
-  //   if (jssStyles) {
-  //     jssStyles.parentElement.removeChild(jssStyles);
-  //   }
-  // }, []);
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
     <>
